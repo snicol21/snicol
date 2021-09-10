ee1e6cb981f62c63a8bde81d84a96a139e242ad2
HQCATALYST\spencer.nicol <spencer.nicol@healthcatalyst.com>
2021-09-10 12:31:56 -0600

    add delete column, delete row, and copy to clipboard the script

--- components/modules/scripts/TableValueConstructor.tsx
+++ components/modules/scripts/TableValueConstructor.tsx
@@ -1,29 +1,28 @@
-import { PlusSmIcon as PlusSmIconSolid } from "@heroicons/react/solid"
-import { ChangeEvent, useEffect, useState } from "react"
+import { PlusIcon, TrashIcon } from "@heroicons/react/solid"
+import { ChangeEvent, useState } from "react"
 
 type IState = {
   columns: string[]
   values: Array<string[]>
 }
 
-const defaultState: IState = {
-  columns: ["Field1", "Field2", "Field3"],
-  values: [
-    ["1", "2", "3"],
-    ["2", "3", "4"],
-    ["3", "4", "5"],
-    ["4", "5", "6"],
-  ],
-}
-
 const TableValueConstructor = () => {
-  const [table, setTable] = useState(defaultState)
+  const [isCopied, setIsCopied] = useState(false)
+  const [table, setTable] = useState({
+    columns: ["Player", "Team", "Number"],
+    values: [
+      ["Michael Jordan", "Chicago Bulls", "23"],
+      ["John Stockton", "Utah Jazz", "12"],
+      ["Larry Bird", "Boston Celtics", "33"],
+      ["Kobe Bryant", "Los Angeles Lakers", "24"],
+    ],
+  } as IState)
 
   const spaces = " ".repeat(5)
   const alias = "tbl"
 
   const script = `SELECT\n${spaces}${alias}.[${table.columns.join(`],\n${spaces}${alias}.[`)}]\nFROM (\n${spaces}VALUES${spaces}${table.values.map(
-    (value) => `\n${spaces}(${value.join(",")})`
+    (value) => `\n${spaces}(${value.map((val) => (isNaN(val as any) && val !== "NULL" ? `'${val}'` : val)).join(", ")})`
   )}\n) AS ${alias} ([${table.columns.join("],[")}])`
 
   const onClickAddColumn = () => {
@@ -35,6 +34,20 @@ const TableValueConstructor = () => {
 
   const onClickAddRow = () => {
     setTable({ ...table, values: table.values.concat([Array(table.columns.length).fill("NULL")]) })
+  }
+
+  const onClickDeleteColumn = (i: number) => {
+    const newColumns = [...table.columns]
+    const newValues = [...table.values]
+    newColumns.splice(i, 1)
+    newValues.map((row) => row.splice(i, 1))
+    setTable({ columns: newColumns, values: newValues })
+  }
+
+  const onClickDeleteRow = (i: number) => {
+    const newValues = [...table.values]
+    newValues.splice(i, 1)
+    setTable({ ...table, values: newValues })
   }
 
   const onChangeColumn = (e: ChangeEvent<HTMLInputElement>, i: number) => {
@@ -49,73 +62,123 @@ const TableValueConstructor = () => {
     setTable({ ...table, values: newValues })
   }
 
+  async function copyTextToClipboard(text) {
+    if ("clipboard" in navigator) {
+      return await navigator.clipboard.writeText(text)
+    } else {
+      return document.execCommand("copy", true, text)
+    }
+  }
+
+  const handleCopyClick = () => {
+    copyTextToClipboard(script)
+      .then(() => {
+        setIsCopied(true)
+        setTimeout(() => {
+          setIsCopied(false)
+        }, 1500)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
   return (
     <div>
-      <button
-        type="button"
-        className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-red-700 bg-red-100 hover:bg-red-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
-        onClick={() =>
-          setTable({
-            columns: ["Field1", "Field2", "Field3"],
-            values: [
-              ["1", "2", "3"],
-              ["2", "3", "4"],
-              ["3", "4", "5"],
-              ["4", "5", "6"],
-            ],
-          })
-        }
-      >
-        Reset
-      </button>
       <div className="overflow-x-auto">
+        <div className="flex w-full place-content-end">
+          <button
+            type="button"
+            className="m-2 inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            onClick={() =>
+              setTable({
+                columns: ["Player", "Team", "Number"],
+                values: [
+                  ["Michael Jordan", "Chicago Bulls", "23"],
+                  ["John Stockton", "Utah Jazz", "12"],
+                  ["Larry Bird", "Boston Celtics", "33"],
+                  ["Kobe Bryant", "Los Angeles Lakers", "24"],
+                ],
+              })
+            }
+          >
+            <span className="w-10">Reset</span>
+          </button>
+        </div>
         <div className="flex my-5">
           <div className="flex flex-col">
             <table>
               <thead className="align-middle">
                 <tr>
                   {table.columns.map((column, columnIndx) => (
-                    <th key={columnIndx} scope="col" className="px-6 text-left  tracking-wider">
-                      <input type="text" value={column} className="p-1 w-full font-medium dark:bg-mat-black" onChange={(e) => onChangeColumn(e, columnIndx)} />
+                    <th key={columnIndx} scope="col" className="px-6 text-left tracking-wider">
+                      <div className="flex flex-col">
+                        <button
+                          type="button"
+                          className="self-center place-content-center inline-flex w-11/12 mb-2 items-center px-2.5 py-1.5 text-xs border dark:border-gray-800 font-medium rounded-xl text-gray-500 bg-gray-50 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-900 dark:hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                          onClick={() => onClickDeleteColumn(columnIndx)}
+                        >
+                          <TrashIcon className="h-5 w-5" aria-hidden="true" />
+                        </button>
+                        <input
+                          type="text"
+                          value={column}
+                          className="p-1 w-full font-medium dark:bg-mat-black"
+                          onChange={(e) => onChangeColumn(e, columnIndx)}
+                        />
+                      </div>
                     </th>
                   ))}
                 </tr>
               </thead>
               <tbody>
                 {table.values.map((valueRow, valueRowIdx) => (
-                  <tr key={valueRowIdx}>
+                  <tr key={valueRowIdx} className="relative">
                     {valueRow.map((value, valueIdx) => (
-                      <td key={valueIdx} className="px-6 py-4 whitespace-nowrap">
-                        <input
-                          type="text"
-                          value={value}
-                          className="p-1 w-full dark:bg-mat-black"
-                          onChange={(e) => onChangeRowValue(e, valueRowIdx, valueIdx)}
-                        />
+                      <td key={valueIdx} className={`px-6 py-4 whitespace-nowrap ${valueRowIdx % 2 === 0 ? "bg-transparent" : "bg-gray-100 dark:bg-gray-700"}`}>
+                        <input type="text" value={value} className="p-1 w-full bg-transparent" onChange={(e) => onChangeRowValue(e, valueRowIdx, valueIdx)} />
                       </td>
                     ))}
+                    <td className="absolute right-0">
+                      <button
+                        type="button"
+                        className="self-center place-content-center inline-flex w-11/12 mb-2 items-center px-2.5 py-1.5 text-xs font-medium rounded-xl text-gray-500 hover:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        onClick={() => onClickDeleteRow(valueRowIdx)}
+                      >
+                        <TrashIcon className="h-5 w-5" aria-hidden="true" />
+                      </button>
+                    </td>
                   </tr>
                 ))}
               </tbody>
             </table>
             <button
               type="button"
-              className="text-primary bg-gray-200 hover:bg-gray-300 dark:bg-gray-800 dark:hover:bg-gray-700 rounded-xl mt-1 p-1"
+              className="text-xs ml-1 mt-2 p-1 border dark:border-gray-800 font-medium rounded-xl text-gray-500 bg-gray-50 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-900 dark:hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               onClick={onClickAddRow}
             >
-              <PlusSmIconSolid className="h-5 w-5 m-auto" aria-hidden="true" />
+              <PlusIcon className="h-5 w-5 m-auto" aria-hidden="true" />
             </button>
           </div>
           <button
             type="button"
-            className="text-primary bg-gray-200 hover:bg-gray-300 dark:bg-gray-800 dark:hover:bg-gray-700 rounded-xl ml-1 p-1 mb-6"
+            className="text-xs mr-1 ml-2 p-1 mb-6 border dark:border-gray-800 font-medium rounded-xl text-gray-500 bg-gray-50 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-900 dark:hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             onClick={onClickAddColumn}
           >
-            <PlusSmIconSolid className="h-5 w-5" aria-hidden="true" />
+            <PlusIcon className="h-5 w-5" aria-hidden="true" />
           </button>
         </div>
       </div>
-      <pre>{script}</pre>
+      <div className="relative">
+        <button
+          type="button"
+          className="absolute right-0 m-2 inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          onClick={handleCopyClick}
+        >
+          <span className="w-10">{isCopied ? "Copied!" : "Copy"}</span>
+        </button>
+        <pre>{script}</pre>
+      </div>
     </div>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../shared/globals.css"
+import Head from "next/head"
 import { ThemeProvider } from "next-themes"
 import { MDXProvider } from "@mdx-js/react"
 import TableValueConstructor from "../components/modules/scripts/TableValueConstructor"
@@ -7,11 +8,16 @@ const components = { TableValueConstructor }
 
 function MyApp({ Component, pageProps }) {
   return (
-    <MDXProvider components={components}>
-      <ThemeProvider attribute="class">
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </MDXProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
+      </Head>
+      <MDXProvider components={components}>
+        <ThemeProvider attribute="class">
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </MDXProvider>
+    </>
   )
 }
 
