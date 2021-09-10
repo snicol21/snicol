9fdbdcd928f99c48966e5e88d11e33e8574f26d4
HQCATALYST\spencer.nicol <spencer.nicol@healthcatalyst.com>
2021-09-10 09:15:07 -0600

    add a meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,7 @@ class MyDocument extends Document {
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
           <link href="https://rsms.me/inter/inter.css" rel="stylesheet" />
         </Head>
         <body className="bg-white dark:bg-mat-black text-gray-900 dark:text-white">
