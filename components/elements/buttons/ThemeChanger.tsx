9a77aaa8c3d6c222e911c7a66d33e4577c24a6e7
HQCATALYST\spencer.nicol <spencer.nicol@healthcatalyst.com>
2021-09-29 15:48:45 -0600

    simplify the theme changer

--- components/elements/buttons/ThemeChanger.tsx
+++ components/elements/buttons/ThemeChanger.tsx
@@ -1,73 +1,25 @@
 import { useEffect, useState } from "react"
 import { useTheme } from "next-themes"
-import { Switch } from "@headlessui/react"
 import { MoonIcon, SunIcon } from "@heroicons/react/solid"
-import { classNames } from "../../../shared/utils/class.util"
 
 const ThemeChanger = () => {
+  const { systemTheme, theme, setTheme } = useTheme()
   const [mounted, setMounted] = useState(false)
-  const [isDark, setIsDark] = useState(false)
-  const { theme, setTheme } = useTheme()
 
   useEffect(() => setMounted(true), [])
-  useEffect(() => {
-    setTheme(theme)
-    if (theme === "dark" && !isDark) {
-      setIsDark(true)
-    }
-  }, [theme, isDark, setTheme, setIsDark])
 
-  const onChangeTheme = () => {
-    if (isDark) {
-      setTheme("light")
-      setIsDark(false)
-      return
+  const renderThemeChange = () => {
+    if (!mounted) return null
+
+    const currentTheme = theme === "system" ? systemTheme : theme
+    if (currentTheme === "dark") {
+      return <MoonIcon className="w-7 h-7 text-blue-500" role="button" onClick={() => setTheme("light")} />
+    } else {
+      return <SunIcon className="w-7 h-7 text-yellow-500" role="button" onClick={() => setTheme("dark")} />
     }
-    setTheme("dark")
-    setIsDark(true)
   }
 
-  if (!mounted) {
-    return null
-  }
-
-  return (
-    <Switch
-      checked={isDark}
-      onChange={onChangeTheme}
-      className={classNames(
-        isDark ? "bg-gray-700" : "bg-gray-200",
-        "relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary"
-      )}
-    >
-      <span className="sr-only">Use Theme</span>
-      <span
-        className={classNames(
-          isDark ? "translate-x-5" : "translate-x-0",
-          "pointer-events-none relative inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"
-        )}
-      >
-        <span
-          className={classNames(
-            isDark ? "opacity-0 ease-out duration-100" : "opacity-100 ease-in duration-200",
-            "absolute inset-0 h-full w-full flex items-center justify-center transition-opacity"
-          )}
-          aria-hidden="true"
-        >
-          <SunIcon className="h-4 w-4 text-yellow-500" />
-        </span>
-        <span
-          className={classNames(
-            isDark ? "opacity-100 ease-in duration-200" : "opacity-0 ease-out duration-100",
-            "absolute inset-0 h-full w-full flex items-center justify-center transition-opacity"
-          )}
-          aria-hidden="true"
-        >
-          <MoonIcon className="h-4 w-4 text-blue-500" />
-        </span>
-      </span>
-    </Switch>
-  )
+  return <div>{renderThemeChange()}</div>
 }
 
 export default ThemeChanger
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
       </Head>
       <MDXProvider components={components}>
-        <ThemeProvider attribute="class">
+        <ThemeProvider attribute="class" enableSystem={true}>
           <Component {...pageProps} />
         </ThemeProvider>
       </MDXProvider>
