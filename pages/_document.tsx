6780b8f421f836e498f2a9b7842569fc979f101f
Spencer Nicol <Spencer.Nicol@gmail.com>
2023-03-24 00:05:46 -0600

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,7 @@ class MyDocument extends Document {
           <link rel="manifest" href="/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff00" />
         </Head>
         <body className="bg-white dark:bg-mat-black text-gray-900 dark:text-white">
           <Main />
